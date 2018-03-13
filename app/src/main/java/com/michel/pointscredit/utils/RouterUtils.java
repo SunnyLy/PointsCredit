3d37d35ebba4e9f3121430950dc96cc05c5105df
SunnyLy <450512747@qq.com>
2018-03-13 20:32:40 +0800

    新建登录注册界面 。。。。。

---
+++ app/src/main/java/com/michel/pointscredit/utils/RouterUtils.java
@@ -0,0 +1,9 @@
+package com.michel.pointscredit.utils;
+
+/**
+ * Created by 80010651 on 2018/3/13.
+ * 页面路由跳转工具类
+ */
+
+public class RouterUtils {
+}
---
+++ app/src/main/java/com/michel/pointscredit/view/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.michel.pointscredit.view.activity;
+
+import com.michel.pointscredit.R;
+import com.michel.pointscredit.base.PCBaseActivity;
+
+/**
+ * Created by 80010651 on 2018/3/13.
+ * 登录界面
+ */
+
+public class LoginActivity extends PCBaseActivity {
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_login;
+    }
+}
