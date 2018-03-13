508f71e61a1cace3f84e7f885d9eb76abdf46c6c
SunnyLy <450512747@qq.com>
2018-03-13 13:42:06 +0800

    添加一个PCApplication。

---
+++ app/src/main/java/com/michel/pointscredit/PCApplication.kt
@@ -0,0 +1,13 @@
+package com.michel.pointscredit
+
+import android.app.Application
+
+/**
+ * Created by 80010651 on 2018/3/13.
+ */
+class PCApplication :Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
